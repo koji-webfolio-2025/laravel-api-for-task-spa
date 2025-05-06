31100a9a3d7234e88728098a816f5bf0f1c38817
koji-webfolio-2025 <codeshift2025@gmail.com>
2025-05-07 08:01:46 +0900

    fix: add sanctum/csrf-cookie route to support cookie-based auth

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::middleware('web')->post('/login', [AuthController::class, 'login']);
+
+Route::get('/sanctum/csrf-cookie', function () {
+    return response()->noContent();
+});
