568a8b05a25b9a050b53be749727e478da280ba2
koji-webfolio-2025 <codeshift2025@gmail.com>
2025-05-07 08:40:50 +0900

    fix: add HandleCors middleware for CORS support

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -6,6 +6,10 @@ use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
 {
+    protected $middleware = [
+        \Illuminate\Http\Middleware\HandleCors::class,
+    ];
+
     protected $middlewareGroups = [
         'web' => [
             \App\Http\Middleware\VerifyCsrfToken::class,
