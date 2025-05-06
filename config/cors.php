19c95ebaa798bd2723350ea0cfd433b5edd142e4
koji-webfolio-2025 <codeshift2025@gmail.com>
2025-05-07 00:59:48 +0900

    fix: set APP_KEY and CORS for production auth

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'login', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173'], // Viteのポート
+    'allowed_origins' => ['http://localhost:5173', 'https://vue3-typescript-task-app.vercel.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
