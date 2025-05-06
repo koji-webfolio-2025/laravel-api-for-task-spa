904dcb40caf211a7dd2278776f540ece60ea58f7
koji-webfolio-2025 <codeshift2025@gmail.com>
2025-05-07 08:27:46 +0900

    Laravel12環境に合わせてCORS設定を修正（Fruitcakeミドルウェア削除）

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,14 +3,9 @@
 namespace App\Http;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
-use Fruitcake\Cors\HandleCors;
 
 class Kernel extends HttpKernel
 {
-    protected $middleware = [
-        \Fruitcake\Cors\HandleCors::class,
-    ];
-
     protected $middlewareGroups = [
         'web' => [
             \App\Http\Middleware\VerifyCsrfToken::class,
