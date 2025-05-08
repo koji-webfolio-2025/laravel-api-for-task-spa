302e551427a2a426652895f29367955e77fb7c1b
koji-webfolio-2025 <codeshift2025@gmail.com>
2025-05-08 21:54:03 +0900

    fix: relax CORS policy for testing

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'login', 'logout', 'csrf-token', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173', 'https://vue3-typescript-task-app.vercel.app'],
+    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
