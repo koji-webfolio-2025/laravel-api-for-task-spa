f68a556e9fd5c00edb0da6e169fc2154cd8aab14
koji-webfolio-2025 <codeshift2025@gmail.com>
2025-05-08 22:31:32 +0900

    fix: use correct CORS middleware for Laravel 12

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Http\Kernel as HttpKernel;
 class Kernel extends HttpKernel
 {
     protected $middleware = [
-        \Fruitcake\Cors\HandleCors::class,
+        \Illuminate\Http\Middleware\HandleCors::class,
     ];
 
     protected $middlewareGroups = [
