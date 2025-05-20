6ea7468ecb50e8459d6309e7c5edd31508c2c1f5
koji-webfolio-2025 <codeshift2025@gmail.com>
2025-05-20 21:11:47 +0900

    chore: remove debug /user route and restore grouped routes

--- routes/api.php
+++ routes/api.php
@@ -9,19 +9,8 @@ use Illuminate\Support\Facades\Log;
 
 Route::post('/login', [AuthController::class, 'login']);
 
-/*Route::middleware('auth:api')->group(function () {
+Route::middleware('auth:api')->group(function () {
     Route::get('/me', fn(Request $request) => $request->user());
     Route::get('/user', fn(Request $request) => response()->json($request->user()));
     Route::apiResource('tasks', TaskController::class);
-});*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    $user = $request->user();
-    Log::info('API /user accessed', ['user' => $user]);
-
-    if (!$user) {
-        return response()->json(['error' => 'Unauthorized (user is null)'], 401);
-    }
-
-    return response()->json($user);
 });
