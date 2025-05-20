b0ef701d08f7d089c62e9bb43cdd15f7176521b4
koji-webfolio-2025 <codeshift2025@gmail.com>
2025-05-20 21:50:43 +0900

    feat: enable /api/user route under auth middleware

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,6 @@ use Illuminate\Support\Facades\Log;
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:api')->group(function () {
-    Route::get('/me', fn(Request $request) => $request->user());
     Route::get('/user', fn(Request $request) => response()->json($request->user()));
     Route::apiResource('tasks', TaskController::class);
 });
